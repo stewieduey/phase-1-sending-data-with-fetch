2ab3516ac9eee9bea7852fa860072b430ef58aae
DuMark Allman <dumark.allman@yahoo.com>
2023-04-12 12:38:53 -0400

    [StewieDuey]

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+const submitData = {
+    name: " ",
+    email: " ",
+  };
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(submitData),
+  };
+  
+ fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+      alert("You have failed this city!!!");
+      console.log(error.message);
+    });
